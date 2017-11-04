8be1f1f37a3dc746a5cd4ab522399b34b8d88904
KinoAR <kinoarose@gmail.com>
2017-11-04 15:33:21 -0400

    Remove tokens

--- bin/clientToken.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function clientID() {
-    return 'Mzc2NDI5MzM0Mzc1OTU2NDgx.DN-Qsw.f-Mws6l8RxFqIwHB7cqLoVFZWIQ';
-}
-exports.clientID = clientID;
-//# sourceMappingURL=clientToken.js.map
